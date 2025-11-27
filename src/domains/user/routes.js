7486577301b7943e6d5303481e831e907bacf420
Naftsira <theo.sirait28@gmail.com>
2025-11-28 05:52:42 +0700

    security update

--- src/domains/user/routes.js
+++ src/domains/user/routes.js
@@ -44,7 +44,9 @@ router.post("/login", async (req, res) => {
 
     const authenticatedUser = await authenticateUser({ email, password });
     res.status(200).cookie("token", authenticatedUser.token, { httpOnly: true, secure: true, sameSite: "strict", path: "/" }).json({ success: true });
-  } catch (error) {}
+  } catch (error) {
+    res.status(403).json({ success: false });
+  }
 });
 
 // Sign up
