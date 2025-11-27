6074a1c36996bae45732330804cf753824d1c19b
Naftsira <theo.sirait28@gmail.com>
2025-11-28 05:05:43 +0700

    preview_1_addrailway_1

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const app = express();
 // render engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use("/static", express.static(path.join(process.cwd(), "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cookieParser());
 app.use(cors());
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const app = require("../src/app");
-const { PORT } = process.env;
+const PORT = process.env.PORT || 3000;
 
 const startApp = () => {
   app.listen(PORT, () => {
